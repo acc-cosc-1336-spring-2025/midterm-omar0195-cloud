5fa67cb6988b55984b7d7b469049fd5f920f5c95
omar0195-cloud <omar.alvarado@g.austincc.edu>
2025-04-04 12:19:18 +0000

    Question 1

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,14 @@
-#add import
+from question_a import reverse_string
+
+def main():
+
+    while True:
+
+        user_input = input("Enter reverse string ")
+
+        result_reversed = reverse_string(user_input)
+        print(f"Reverse String is: {result_reversed}")
+
+    
+            
+main()
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,13 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+def reverse_string(string1):
+    reversed_st = ""
+    index = len(string1) - 1
+
+    while index >= 0:
+        reversed_st += string1 [index]
+        index -= 1
+
+    return reversed_st
