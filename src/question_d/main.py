3513f944c4705808f43a98262b047cb1b3819e1f
omar0195-cloud <omar.alvarado@g.austincc.edu>
2025-04-05 04:26:25 +0000

    Question 4

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,1 +1,21 @@
 #add import
+from question_d import get_assessment_value, get_tax_assessed
+
+if __name__ == "__main__":
+    while True:
+        try:
+            user_input = input("Enter value of the property or 'q' to exit the program: ")
+            if user_input.lower() == 'q':
+                print("Exiting Program")
+                break
+            
+            value_of_property = float(user_input)
+            assessment_value = get_assessment_value(value_of_property)
+            tax = get_tax_assessed(assessment_value)
+
+            print(f"Assessment Value is  ${assessment_value:.2f}")
+            print(f"Property Tax is ${tax:2f}")
+
+        except ValueError:
+            print("Invalid input, enter a number for the property value")
+
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -5,6 +5,7 @@ import unittest
 from src.question_a.question_a import test_config
 from src.question_b.question_b import get_day_of_the_week
 from src.question_c.question_c import use_global, get_global_num
+from src.question_d.question_d import get_assessment_value, get_tax_assessed
 
 class Test_Config(unittest.TestCase):
 
@@ -19,3 +20,11 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(get_global_num(), 25, "Initial value of value of global_num is 25")
         use_global()
         self.assertEqual(get_global_num(), 10, "Global_num should be 10 now") 
+
+    def test_get_assessment_value(self):
+        self.assertEqual(get_assessment_value(10000), 6000)
+        self.assertEqual(get_assessment_value(20000), 12000)
+    
+    def test_get_tax_assessed(self):
+        self.assertAlmostEqual(get_tax_assessed(6000), 43.20, places= 2)
+        self.assertAlmostEqual(get_tax_assessed(10000), 72.00, places= 2)
